b27029710ea40b117f5eee7960b20be15370a1ca
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-18 11:28:47 -0400

    code challenge - randomShuffle - complete and functioning

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -1,2 +1,15 @@
 import UIKit
 
+func randomShuffle(array: inout[Int]) {
+    for _ in 0..<array.count {
+        let oldIndex = Int.random(in: 0..<array.count)
+        let newIndex = Int.random(in: 0..<array.count)
+        let entry = array[oldIndex]
+        array.remove(at: oldIndex)
+        array.insert(entry, at: newIndex)
+    }
+}
+
+
+var arrayToShuffle = [6,7,8,9]
+randomShuffle(array: &arrayToShuffle)
