827c05a783c87157ddda9f2a199d29fda2718f18
Kerby Jean <kerby.jean@hotmail.fr>
2020-03-25 11:39:38 -0400

    Done

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -1,2 +1,13 @@
 import UIKit
 
+
+func randomShuffle(array: [Int]) -> [Int]? {
+    var array = array
+    let randomNumber = Int(arc4random_uniform(UInt32(array.count)))
+    let secondRandomNumber = Int(arc4random_uniform(UInt32(array.count)))
+    array.swapAt(randomNumber, secondRandomNumber)
+    return array
+
+}
+print(randomShuffle(array: [2, 3, 7, 8]))
+
