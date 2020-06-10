e59d9fb231269542f53793a7aa782e174c1dce3b
Cody Morley <codymorleyinc@gmail.com>
2020-06-10 08:53:43 -0700

    Not sure what i'm doing after this point. Can't find a handle.

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -1,2 +1,11 @@
 import UIKit
 
+func randomShuffle(_ array: inout Array<Any>) {
+    var rangeOfIndicies: ClosedRange = 1...array.count
+    
+    
+    for value in array {
+        let valueSub = value
+        array.remove
+    }
+}
