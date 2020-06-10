3b1fa205de558e582d2fddf6eab0465a45833ec9
Joe V <joeveverka89@gmail.com>
2020-06-10 12:17:11 -0400

    Got it

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -1,2 +1,11 @@
 import UIKit
 
+func randomShuffle(array: [Int]) -> [Int] {
+    var array = array
+    let randomNum = Int(arc4random_uniform(UInt32(array.count)))
+    let randomNumTwo = Int(arc4random_uniform(UInt32(array.count)))
+    array.swapAt(randomNum, randomNumTwo)
+    return array
+
+}
+print(randomShuffle(array: [1, 2, 43, 9, 10]))
