120524bd9cd0c4f07202d050a552734131b34239
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-10 10:07:25 -0600

    done

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -1,2 +1,18 @@
 import UIKit
 
+
+func randomShuffle(numbers: inout [Int]) {
+    
+    for _ in 0..<numbers.count {
+        let oldIndex = Int.random(in: 0..<numbers.count)
+        let newIndex = Int.random(in: 0..<numbers.count)
+        
+        let entryPoint = numbers[oldIndex]
+        numbers.remove(at: oldIndex)
+        numbers.insert(entryPoint, at: newIndex)
+        
+    }
+}
+
+var arrayToShuffle = [6,7,8,9]
+randomShuffle(numbers: &arrayToShuffle)
