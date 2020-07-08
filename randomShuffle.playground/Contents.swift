51ec651e5bc128b64a32fb4630d2085af5681b8a
Harmony Radley <harmonyradley99@gmail.com>
2020-07-08 12:00:42 -0400

    not finished

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -2,7 +2,9 @@ import UIKit
 
 func randomInt(floor: Int, ceiling: Int) -> Int {
 
+    let randomNumber = arc4random_uniform(UIInt32)
 
-    // I could not figure this out. 
+    // still couldn't figure this out..
+
 }
 
