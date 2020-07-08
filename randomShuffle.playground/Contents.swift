a07fabecd997f70c4da49c60496dc439d3d8f20d
Cody Morley <codymorleyinc@gmail.com>
2020-07-08 08:59:49 -0700

    Mostly written but not working

--- randomShuffle.playground/Contents.swift
+++ randomShuffle.playground/Contents.swift
@@ -2,10 +2,23 @@ import UIKit
 
 func randomShuffle(_ array: inout Array<Any>) {
     var rangeOfIndicies: ClosedRange = 1...array.count
+    var shuffler: Array<Any> = []
     
-    
-    for value in array {
-        let valueSub = value
-        array.remove
+    for index in rangeOfIndicies {
+        shuffler.append(index)
     }
+    
+    for element in array {
+        let replacementIndex = Int.random(in: rangeOfIndicies)
+        shuffler.remove(at: replacementIndex)
+        shuffler.insert(element, at: replacementIndex)
+    }
+    array = shuffler
 }
+
+
+var namesArray = ["Opie", "Anthony", "Jimmy", "Sam"]
+var intsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+randomShuffle(intsArray)
+print(intsArray)
